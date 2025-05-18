a2a5db4c4cbee258ca1d1a424d8a7409c79e4d44
Sujith Kani <sujithkani2004@gmail.com>
2025-05-18 22:49:43 +0530

    Update top.v

--- uart_rx/top.v
+++ uart_rx/top.v
@@ -1,21 +1,15 @@
-module uart_rx_top (
-    input wire clk,
-    input wire rst_n,
-    input wire rx,
-    output wire [5:0] led
-);
+module uart_rx_top(input wire clk,rst_n,rx,
+    output wire [5:0] led);
 
-    parameter CLK_FREQ = 27000000;
-    parameter BAUD_RATE = 115200;
-
+    parameter CLK_FREQ=27000000;
+    parameter BAUD_RATE=115200;
     wire [7:0] rx_data;
     wire data_ready;
     reg [5:0] led_val;
 
     uart_rx_8bit #(.CLK_FREQ(CLK_FREQ), .BAUD_RATE(BAUD_RATE)) 
     uart_rx_inst(.clk(clk),.rst_n(rst_n),.rx(rx),.data_out(rx_data),.data_ready(data_ready));
-
-    //LED register interface (maps received value to LED)
+    
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n)
             led_val<=6'b000000;
